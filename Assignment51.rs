b10efeed6ec7b64ab90721aedc017deeba5d7e4b
Divyanshh112 <joel.yt987@gmail.com>
2024-05-17 20:07:37 +0530

    Create Assignment51.rs

---
+++ Assignment51.rs
@@ -0,0 +1,6 @@
+fn array_and_vec() -> ([i32; 4], Vec<i32>) {
+    let a = [10, 20, 30, 40]; // a plain array
+    let v = vec![10, 20, 30, 40]; // Declare a vector using the macro for vectors
+
+    (a, v)
+}
