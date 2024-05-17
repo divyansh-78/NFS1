aeb07987009b8cdfe89e0596e4717b977031929b
Divyanshh112 <joel.yt987@gmail.com>
2024-05-17 20:54:09 +0530

    Create Assignment41.rs

---
+++ Assignment41.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // Booleans (`bool`)
+
+    let is_morning = true;
+    if is_morning {
+        println!("Good morning!");
+    }
+
+    let is_evening = false; // Declare and initialize is_evening with false
+    if is_evening {
+        println!("Good evening!");
+    }
+}
