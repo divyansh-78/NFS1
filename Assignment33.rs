27f56f8c38df2a7f5dadf1909cf77265df52a04a
Divyanshh112 <joel.yt987@gmail.com>
2024-05-17 21:13:28 +0530

    Create Assignment33.rs

---
+++ Assignment33.rs
@@ -0,0 +1,24 @@
+pub fn animal_habitat(animal: &str) -> &'static str {
+    let identifier = if animal == "crab" {
+        1
+    } else if animal == "gopher" {
+        2
+    } else if animal == "snake" {
+        3
+    } else {
+        0
+    };
+
+    // DO NOT CHANGE THIS STATEMENT BELOW
+    let habitat = if identifier == 1 {
+        "Beach"
+    } else if identifier == 2 {
+        "Burrow"
+    } else if identifier == 3 {
+        "Desert"
+    } else {
+        "Unknown"
+    };
+
+    habitat
+}
