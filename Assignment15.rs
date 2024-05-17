d9cca10b53a9f9f8791df7c597d8c93ce3d6396f
Divyanshh112 <joel.yt987@gmail.com>
2024-05-17 21:05:41 +0530

    Create Assignment15.rs

---
+++ Assignment15.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut number = 3; // Change the type to i32 and make it mutable
+    println!("Spell a Number : {}", number); // Print the number as a string
+    number += 2; // Add 2 to the number
+    println!("Number plus two is : {}", number); // Print the number plus two
+}
